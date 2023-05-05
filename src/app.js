db79268555125625e9fe7eb3ac5bf91a0ecd0df2
Khaleel Ibrahim <alhajikhaleel@gmail.com>
2023-05-06 04:43:39 +0530

    add active link effect on scroll and on click

--- src/app.js
+++ src/app.js
@@ -1,20 +1,30 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   // const navLinks = document.querySelectorAll('.nav-link')
-//   // navLinks.forEach(function (navLink) {
-//   //     navLink.addEventListener('click', function () {
-//   //         navLink.classList.toggle('active');
-//   //     })
-//   // })
-//   // Add active class to the current button (highlight it)
-//   const header = document.getElementById("myDIV");
-//   const navLinks = header.getElementsByClassName("nav-link");
-//   for (let i = 0; i < navLinks.length; i++) {
-//     navLinks[i].addEventListener("click", function () {
-//       const current = document.getElementsByClassName("active");
-//       if (current.length > 0) {
-//         current[0].className = current[0].className.replace(" active", "");
-//       }
-//       this.className += " active";
-//     });
-//   }
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  // Add active class to the current section on scroll
+  const navLinks = document.querySelectorAll("#ul .nav-link");
+  const sections = document.querySelectorAll("section");
+  const options = {
+    rootMargin: "-50px",
+    threshold: 0.5,
+  };
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const index = [...sections].indexOf(entry.target);
+        navLinks.forEach((link) => link.classList.remove("active"));
+        navLinks[index].classList.add("active");
+      }
+    });
+  }, options);
+  sections.forEach((section) => observer.observe(section));
+
+  // Add active class to the current section on click
+  const header = document.getElementById("ul");
+  const navLink = header.getElementsByClassName("nav-link");
+  for (let i = 0; i < navLinks.length; i++) {
+    navLink[i].addEventListener("click", function () {
+      const current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+});
