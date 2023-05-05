2232f7890629e6246141c75875cc7d2a410673d2
Khaleel Ibrahim <alhajikhaleel@gmail.com>
2023-05-06 05:05:34 +0530

    modify scroll effects

--- src/app.js
+++ src/app.js
@@ -1,30 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add active class to the current section on scroll
-  const navLinks = document.querySelectorAll("#ul .nav-link");
-  const sections = document.querySelectorAll("section");
-  const options = {
-    rootMargin: "-50px",
-    threshold: 0.5,
-  };
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const index = [...sections].indexOf(entry.target);
-        navLinks.forEach((link) => link.classList.remove("active"));
-        navLinks[index].classList.add("active");
-      }
-    });
-  }, options);
-  sections.forEach((section) => observer.observe(section));
+    const navLinks = document.querySelectorAll("#ul .nav-link");
+    const sections = document.querySelectorAll("section");
+    const options = {
+      rootMargin: "-50px",
+      threshold: 0.5,
+    };
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = [...sections].indexOf(entry.target);
+          navLinks.forEach((link) => link.classList.remove("active"));
+          navLinks[index].classList.add("active");
+        }
+      });
+    }, options);
+    sections.forEach((section) => observer.observe(section));
 
-  // Add active class to the current section on click
-  const header = document.getElementById("ul");
-  const navLink = header.getElementsByClassName("nav-link");
-  for (let i = 0; i < navLinks.length; i++) {
-    navLink[i].addEventListener("click", function () {
-      const current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
+    // Add active class to the current section on click
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        navLinks.forEach((link) => link.classList.remove("active"));
+        link.classList.add("active");
+      });
     });
-  }
 });
