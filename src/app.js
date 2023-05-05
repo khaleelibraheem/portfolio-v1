b850ede4086c20a2298e8018a3e08753ce1f7f65
Khaleel Ibrahim <alhajikhaleel@gmail.com>
2023-05-06 03:24:04 +0530

    add scroll effect

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   // const navLinks = document.querySelectorAll('.nav-link')
+//   // navLinks.forEach(function (navLink) {
+//   //     navLink.addEventListener('click', function () {
+//   //         navLink.classList.toggle('active');
+//   //     })
+//   // })
+//   // Add active class to the current button (highlight it)
+//   const header = document.getElementById("myDIV");
+//   const navLinks = header.getElementsByClassName("nav-link");
+//   for (let i = 0; i < navLinks.length; i++) {
+//     navLinks[i].addEventListener("click", function () {
+//       const current = document.getElementsByClassName("active");
+//       if (current.length > 0) {
+//         current[0].className = current[0].className.replace(" active", "");
+//       }
+//       this.className += " active";
+//     });
+//   }
+// });
