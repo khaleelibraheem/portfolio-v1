fec526068dc87560af17b64f132b248ed249b45d
Khaleel Ibrahim <alhajikhaleel@gmail.com>
2023-05-06 19:58:47 +0530

    fix section animations

--- src/app.js
+++ src/app.js
@@ -1,27 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Add active class to the current section on scroll
-    const navLinks = document.querySelectorAll("#ul .nav-link");
-    const sections = document.querySelectorAll("section");
-    const options = {
-      rootMargin: "-50px",
-      threshold: 0.5,
-    };
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const index = [...sections].indexOf(entry.target);
-          navLinks.forEach((link) => link.classList.remove("active"));
-          navLinks[index].classList.add("active");
-        }
-      });
-    }, options);
-    sections.forEach((section) => observer.observe(section));
-
-    // Add active class to the current section on click
-    navLinks.forEach((link) => {
-      link.addEventListener("click", () => {
+  const navLinks = document.querySelectorAll("#ul .nav-link");
+  const sections = document.querySelectorAll("section");
+  const scrollOptions = {
+    rootMargin: "-50px",
+    threshold: 0.5,
+  };
+  const scrollObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const index = [...sections].indexOf(entry.target);
         navLinks.forEach((link) => link.classList.remove("active"));
-        link.classList.add("active");
-      });
+        navLinks[index].classList.add("active");
+      }
     });
+  }, scrollOptions);
+  sections.forEach((section) => scrollObserver.observe(section));
+
+  // Add active class to the current section on click
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.forEach((link) => link.classList.remove("active"));
+      link.classList.add("active");
+    });
+  });
+
+  // Detect when a user has scrolled to a section
+  const sectionOptions = {
+    rootMargin: "0px",
+    threshold: 0.2,
+  };
+
+  const sectionObserver = new IntersectionObserver(function (
+    entries,
+    observer
+  ) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("activate");
+      }
+    });
+  },
+  sectionOptions);
+
+  // Add the observer to each section
+  sections.forEach((section) => {
+    sectionObserver.observe(section);
+  });
 });
